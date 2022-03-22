ef11350c2f6ada2b5f14f6bb67fc8b71bb3f1e64
Jb8hub <jb8git.hub@gmail.com>
2022-03-22 19:46:07 -0300

    Error handler/mongoDB instance

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,13 +4,21 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
-const app = express ();
+const app = express();
 app.use(json());
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Lintening on port 3000!');
