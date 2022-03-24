b8b4a2667edd50567a1d98c65dd863e3c0688d36
Jb8hub <jb8git.hub@gmail.com>
2022-03-24 14:14:01 -0300

    password toHash

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
@@ -15,24 +15,27 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
+    try {
+      if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+      }
 
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+      const { email, password } = req.body;
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        throw new BadRequestError('Email in use');
+      }
+
+      const user = User.build({ email, password });
+      await user.save();
+
+      res.status(201).send(user);
+    } catch (err) {
+      next(err);
     }
-
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      throw new BadRequestError('Email in use');
-    }
-
-    const user = User.build({ email, password });
-    await user.save();
-
-    res.status(201).send(user);
   }
 );
 
