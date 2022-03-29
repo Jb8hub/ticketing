3b065ea45c83be33c50c5931c2b683aeb8d762c1
Jb8hub <jb8git.hub@gmail.com>
2022-03-29 12:36:33 -0300

    Test signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import express, { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { Password } from '../services/password';
 import { User } from '../models/user';
@@ -15,36 +15,41 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must suppy a password'),
   ],
   validateRequest,
-  async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      validationResult(req);
+      const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
+      const existingUser = await User.findOne({ email });
 
-    if (!existingUser) {
-      throw new BadRequestError('Invalid credentials');
+      if (!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+      }
+      const passwordsMatch = await Password.compare(
+        existingUser.password,
+        password
+      );
+      if (!passwordsMatch) {
+        throw new BadRequestError('Invalid Credentials');
+      }
+
+      // Generate JWT I
+      const userJwt = jwt.sign(
+        {
+          id: existingUser.id,
+          email: existingUser.email,
+        },
+        process.env.JWT_KEY!
+      );
+      // Store it on session object
+      req.session = {
+        jwt: userJwt,
+      };
+
+      res.status(200).send(existingUser);
+    } catch (err) {
+      next(err);
     }
-    const passwordsMatch = await Password.compare(
-      existingUser.password,
-      password
-    );
-    if (!passwordsMatch) {
-      throw new BadRequestError('Invalid Credentials');
-    }
-
-    // Generate JWT I
-    const userJwt = jwt.sign(
-      {
-        id: existingUser.id,
-        email: existingUser.email,
-      },
-      process.env.JWT_KEY!
-    );
-    // Store it on session object
-    req.session = {
-      jwt: userJwt,
-    };
-
-    res.status(200).send(existingUser);
   }
 );
 
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,12 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 
 beforeAll(async () => {
+  jest.setTimeout(90 * 1000);
   process.env.JWT_KEY = 'test-auth-key-jwt';
-
   const mongo = await MongoMemoryServer.create();
   const uri = mongo.getUri();
   await mongoose.connect(uri);
@@ -23,3 +28,19 @@ afterAll(async () => {
   //await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
