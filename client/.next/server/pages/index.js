c10ba06138a60e3b3af3149b60a09da7cecea2a4
Jb8hub <jb8git.hub@gmail.com>
2022-04-13 12:38:35 -0300

    next lint

--- client/.next/server/pages/index.js
+++ client/.next/server/pages/index.js
@@ -1,35 +1,68 @@
 "use strict";
-/*
- * ATTENTION: An "eval-source-map" devtool has been used.
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 (() => {
 var exports = {};
-exports.id = "pages/index";
-exports.ids = ["pages/index"];
+exports.id = 405;
+exports.ids = [405];
 exports.modules = {
 
-/***/ "./pages/index.js":
-/*!************************!*\
-  !*** ./pages/index.js ***!
-  \************************/
+/***/ 201:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst Landing = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Landing Page\"\n    }, void 0, false, {\n        fileName: \"C:\\\\PROJETOS\\\\TICKETING\\\\client\\\\pages\\\\index.js\",\n        lineNumber: 2,\n        columnNumber: 10\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Landing);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsS0FBSyxDQUFDQSxPQUFPLE9BQVMsQ0FBQztJQUNyQixNQUFNLDZFQUFFQyxDQUFFO2tCQUFDLENBQVk7Ozs7OztBQUN6QixDQUFDO0FBRUQsaUVBQWVELE9BQU8sRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2NsaWVudC8uL3BhZ2VzL2luZGV4LmpzP2JlZTciXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgTGFuZGluZyA9ICgpID0+IHtcclxuICByZXR1cm4gPGgxPkxhbmRpbmcgUGFnZTwvaDE+O1xyXG59O1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgTGFuZGluZzsiXSwibmFtZXMiOlsiTGFuZGluZyIsImgxIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ pages)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "axios"
+var external_axios_ = __webpack_require__(167);
+var external_axios_default = /*#__PURE__*/__webpack_require__.n(external_axios_);
+;// CONCATENATED MODULE: ./src/api/build-client.js
+
+/* harmony default export */ const build_client = (({ req  })=>{
+    if (true) {
+        // We are on the server
+        return external_axios_default().create({
+            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            headers: req.headers
+        });
+    } else {}
+});
+
+;// CONCATENATED MODULE: ./src/pages/index.js
+
+
+const LandingPage = ({ currentUser  })=>{
+    console.log(currentUser);
+    return /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+        children: "Landing Page"
+    });
+};
+LandingPage.getInitialProps = async (context)=>{
+    const client = build_client(context);
+    const { data  } = await client.get('/api/users/currentuser');
+    return data;
+};
+/* harmony default export */ const pages = (LandingPage);
+
 
 /***/ }),
 
-/***/ "react/jsx-dev-runtime":
-/*!****************************************!*\
-  !*** external "react/jsx-dev-runtime" ***!
-  \****************************************/
+/***/ 167:
 /***/ ((module) => {
 
-module.exports = require("react/jsx-dev-runtime");
+module.exports = require("axios");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
 
 /***/ })
 
@@ -40,7 +73,7 @@ module.exports = require("react/jsx-dev-runtime");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+var __webpack_exports__ = (__webpack_exec__(201));
 module.exports = __webpack_exports__;
 
 })();
