e080f46af27cb1a34892beec9611e9b06b164d61
Jb8hub <jb8git.hub@gmail.com>
2022-04-13 10:19:17 -0300

    235. A Reusable API Client

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,24 +1,15 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://ingress-nginx.ingress-nginx...
-    const { data } = await axios.get(
-      'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
 };
+
+export default LandingPage;
